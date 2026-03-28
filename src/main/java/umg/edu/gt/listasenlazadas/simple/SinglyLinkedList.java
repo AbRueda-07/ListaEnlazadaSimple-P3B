543fe97d157b74cc9dc1be0e2e8fbf0dd0623402
AbRueda-07 <aruedaa@miumg.edu.gt>
2026-03-27 22:30:47 -0600

    Implementa countOccurrences en SinglyLinkedList

--- src/main/java/umg/edu/gt/listasenlazadas/simple/SinglyLinkedList.java
+++ src/main/java/umg/edu/gt/listasenlazadas/simple/SinglyLinkedList.java
@@ -105,10 +105,20 @@ public class SinglyLinkedList<T> {
      * @return cantidad de ocurrencias encontradas
      */
     public int countOccurrences(T value) {
-        throw new UnsupportedOperationException(
-                "TODO RETO: Implementar countOccurrences(T value) "
-                + "en SinglyLinkedList.");
+        int count = 0;
+        SimpleNode<T> current = head;
+
+        while (current != null) {
+            if (isSameValue(current.getValue(), value)) {
+                count++;
+            }
+            current = current.getNext();
+        }
+
+        return count;
     }
+
+
 
     @Override
     public String toString() {
