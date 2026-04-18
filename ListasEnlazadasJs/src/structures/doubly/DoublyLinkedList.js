fe4798eaa30b5bd6acbfaf70031bdb8f4d5e3de7
AbRueda-07 <aruedaa@miumg.edu.gt>
2026-04-17 22:10:52 -0600

    feat: agregar proyecto de listas enlazadas en JavaScript (estructura base)

---
+++ ListasEnlazadasJs/src/structures/doubly/DoublyLinkedList.js
@@ -0,0 +1,194 @@
+const DoublyNode = require("./DoublyNode");
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this._size = 0;
+  }
+
+  addFirst(value) {
+    const newNode = new DoublyNode(value);
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head.previous = newNode;
+      this.head = newNode;
+    }
+    this._size++;
+  }
+
+  addLast(value) {
+    const newNode = new DoublyNode(value);
+    if (this.tail === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.previous = this.tail;
+      this.tail = newNode;
+    }
+    this._size++;
+  }
+
+  removeFirst() {
+    if (this.head === null) {
+      return null;
+    }
+    const value = this.head.value;
+    this.head = this.head.next;
+    if (this.head === null) {
+      this.tail = null;
+    } else {
+      this.head.previous = null;
+    }
+    this._size--;
+    return value;
+  }
+
+  removeLast() {
+    if (this.tail === null) {
+      return null;
+    }
+    const value = this.tail.value;
+    this.tail = this.tail.previous;
+    if (this.tail === null) {
+      this.head = null;
+    } else {
+      this.tail.next = null;
+    }
+    this._size--;
+    return value;
+  }
+
+  contains(value) {
+    let current = this.head;
+    while (current !== null) {
+      if (this._isSameValue(current.value, value)) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
+  }
+
+  countOccurrences(value) {
+    let count = 0;
+    let current = this.head;
+
+    while (current !== null){
+      if (current.value === value){
+        count++;
+      }
+      current = current.next;
+  }    
+  return count;
+
+  }
+
+  clean() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+}
+
+ reverseInPlace() {
+    if (this.head === null || this.head.next === null) {
+        return;
+    }
+
+    let current = this.head;
+    this.tail = this.head; 
+    let prevNode = null;
+
+    while (current !== null) {
+        let nextNode = current.next; 
+        current.next = prevNode;
+        current.prev = nextNode;
+
+        
+        prevNode = current;
+        current = nextNode;
+    }
+
+    this.head = prevNode;
+}
+
+removeDuplicates() {
+    let current = this.head;
+
+    while (current !== null) {
+        let runner = current.next;
+
+        while (runner !== null) {
+            if (runner.value === current.value) {
+
+                if (runner.prev !== null) {
+                    runner.prev.next = runner.next;
+                }
+
+                if (runner.next !== null) {
+                    runner.next.prev = runner.prev;
+                }
+
+                if (runner === this.tail) {
+                    this.tail = runner.prev;
+                }
+
+                this.size--;
+
+                runner = runner.next;
+
+            } else {
+                runner = runner.next;
+            }
+        }
+
+        current = current.next;
+    }
+}
+
+  size() {
+    return this._size;
+  }
+
+  isEmpty() {
+    return this._size === 0;
+  }
+
+  toForwardString() {
+    let out = "[";
+    let current = this.head;
+    while (current !== null) {
+      out += String(current.value);
+      if (current.next !== null) {
+        out += ", ";
+      }
+      current = current.next;
+    }
+    out += "]";
+    return out;
+  }
+
+  toBackwardString() {
+    let out = "[";
+    let current = this.tail;
+    while (current !== null) {
+      out += String(current.value);
+      if (current.previous !== null) {
+        out += ", ";
+      }
+      current = current.previous;
+    }
+    out += "]";
+    return out;
+  }
+
+  _isSameValue(left, right) {
+    return left === right;
+  }
+}
+
+module.exports = DoublyLinkedList;
---
+++ ListasEnlazadasJs/src/structures/simple/SinglyLinkedList.js
@@ -0,0 +1,145 @@
+const SimpleNode = require("./SimpleNode");
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this._size = 0;
+  }
+
+  addFirst(value) {
+    const newNode = new SimpleNode(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    if (this.tail === null) {
+      this.tail = newNode;
+    }
+    this._size++;
+  }
+
+  addLast(value) {
+    const newNode = new SimpleNode(value);
+    if (this.tail === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this._size++;
+  }
+
+  removeFirst() {
+    if (this.head === null) {
+      return null;
+    }
+    const value = this.head.value;
+    this.head = this.head.next;
+    if (this.head === null) {
+      this.tail = null;
+    }
+    this._size--;
+    return value;
+  }
+
+  contains(value) {
+    let current = this.head;
+    while (current !== null) {
+      if (this._isSameValue(current.value, value)) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
+  }
+
+  //Se realizo modificación al método countOccurrences
+  // A JavaScript
+  countOccurrences(value) {
+   let count = 0;
+   let current = this.head;
+
+   while (current !== null){
+    if(current.value === value){
+      count++;
+    }
+    current = current.next;
+   }
+   return count;
+  }
+
+  clean() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  reverseInPlace() {
+   if(this.head === null || this.head.next === null){
+    return;
+   }
+
+   let previous = null;
+   let current = this.head;
+   let next = null;
+
+   this.tail = this.head;
+
+   while (current !== null){
+    next = current.next;
+    current.next = previous;
+    previous = current;
+    current = next;
+   }
+   this.head = previous;
+  }
+
+  removeDuplicates() {
+    let current = this.head;
+
+    while (current == null){
+      let runner = current;
+
+      while (runner.next !== null){
+        if (runner.next.value === current.value){
+          if (runner.next === this.tail){
+            this.tail = runner;
+          }
+          runner.next = runner.next.next;
+          this.size--;
+        }else{
+          runner = runner.next;
+        }
+      }
+      current = current.next;
+    } 
+   }
+
+  size() {
+    return this._size;
+  }
+
+  isEmpty() {
+    return this._size === 0;
+  }
+
+  toString() {
+    let out = "[";
+    let current = this.head;
+    while (current !== null) {
+      out += String(current.value);
+      if (current.next !== null) {
+        out += ", ";
+      }
+      current = current.next;
+    }
+    out += "]";
+    return out;
+  }
+
+  _isSameValue(left, right) {
+    return left === right;
+  }
+}
+
+module.exports = SinglyLinkedList;
